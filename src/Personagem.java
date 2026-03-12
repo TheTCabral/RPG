61b5056c1eabaecb6babecfe6e9068600f1c3545
Thais <thais.cabral81@gmail.com>
2026-03-12 20:26:13 -0300

    Primeiros Cards

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,4 +1,4 @@
-public class Personagem {
+public abstract class Personagem {
     String nome;
     int vida;
     int forca;
@@ -8,4 +8,11 @@ public class Personagem {
     int experiencia;
     int nivel;
     int pontosParaUpar;
+
+    public Personagem() {}
+    public Personagem(String nome, int vida, int forca, int defesa, int mana, int velocidade, int experiencia, int nivel, int pontosParaUpar) {}
+    public void atacar() {}
+    public void defender() {}
+    public void curar() {}
+    public void habilidadeEspecial() {}
 }
