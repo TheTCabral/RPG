0d4066ef17b8471abcba2838b9c629fb0b316200
Thais <thais.cabral81@gmail.com>
2026-03-12 20:17:48 -0300

    Primeiros Cards

---
+++ src/Personagem.java
@@ -0,0 +1,11 @@
+public class Personagem {
+    String nome;
+    int vida;
+    int forca;
+    int defesa;
+    int mana;
+    int velocidade;
+    int experiencia;
+    int nivel;
+    int pontosParaUpar;
+}
