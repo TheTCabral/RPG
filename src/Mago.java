b3c64b398df373c87e7bffebf87162d4cdcdc28e
Thais <thais.cabral81@gmail.com>
2026-03-12 23:55:34 -0300

    Alguns atributos de personagem

--- src/Mago.java
+++ src/Mago.java
@@ -1,2 +1,3 @@
 public class Mago {
+    int mana;
 }
--- src/Personagem.java
+++ src/Personagem.java
@@ -3,14 +3,14 @@ public abstract class Personagem {
     int vida;
     int forca;
     int defesa;
-    int mana;
     int velocidade;
     int experiencia;
     int nivel;
     int pontosParaUpar;
+    int stamina;
 
     public Personagem() {}
-    public Personagem(String nome, int vida, int forca, int defesa, int mana, int velocidade, int experiencia, int nivel, int pontosParaUpar) {}
+    public Personagem(String nome, int vida, int forca, int defesa, int velocidade, int experiencia, int nivel, int pontosParaUpar) {}
     public void atacar() {}
     public void defender() {}
     public void curar() {}
